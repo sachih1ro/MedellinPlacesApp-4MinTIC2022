70886bbc59786ef1b6a6fceda982c5d8cc80bf5c
Camilo <cmilocarreno09@gmail.com>
2021-11-28 22:28:05 -0500

    Se crea la activity PlacesListActivity.kt junto a su respectivo activity_places_list.xml para mostrar la lista de lugares a través de un RecyclerView. Para ello también se crean la clase adapatadora del RecyclerView CustomPlacesAdapter.kt y una clase Utils.kt que contiene la función para cargar el archivo places.json, también añadido, el cual contiene la descripción de los lugares. También se añaden imágenes de los lugares en la carpeta drawable junto al archivo border_round.xml que sirve para darle un contorno a cada item en el RecyclerView. Finalmente, otros archivos como strings.xml fueron actualizados con nuevos valores a partir de los ítems añadidos.

--- app/src/main/java/com/example/medellinplaces/PlaceItemActivity.kt
+++ app/src/main/java/com/example/medellinplaces/PlaceItemActivity.kt
@@ -1,12 +1,11 @@
 package com.example.medellinplaces
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class PlaceItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //Prueba comentario
+        setContentView(R.layout.activity_place_item)
     }
 }
