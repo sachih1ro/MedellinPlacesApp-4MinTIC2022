96104233cb03f6823cbbfa6237d9778a718098b6
Camilo <cmilocarreno09@gmail.com>
2021-12-18 00:47:13 -0500

    Sprint 4: se implementa el intent para abrir la ubicación del POI en Google Maps

--- app/src/main/java/com/example/medellinplaces/model/PlaceModel.kt
+++ app/src/main/java/com/example/medellinplaces/model/PlaceModel.kt
@@ -5,7 +5,9 @@ import com.google.gson.annotations.SerializedName
 data class PlaceModel(@SerializedName("name") val placeName: String,
                       @SerializedName("description") val placeDescription: String,
                       @SerializedName("image") val imageName: String,
-                      @SerializedName("score") val placeScore: String) {
+                      @SerializedName("score") val placeScore: String,
+                      @SerializedName("latitude") val placeLatitude: String,
+                      @SerializedName("longitude") val placeLongitude: String) {
 }
 
 /*
